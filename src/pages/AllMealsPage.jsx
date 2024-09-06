81efd1dfeeba34b728e471336184041fb8c06bbf
Miracle Ibharokhonre <miracleosemudiahen@hotmail.com>
2024-09-06 10:18:59 +0100

    aligned card items

--- src/pages/AllMealsPage.jsx
+++ src/pages/AllMealsPage.jsx
@@ -1,48 +1,37 @@
-import Card from "../components/Card"
-import { getAllMeals } from "../../api/getAllMeals"
-import { useQuery } from "@tanstack/react-query"
-import SkeletonLoader from "../components/SkeletonLoader"
+import Card from "../components/Card";
+import { getAllMeals } from "../../api/getAllMeals";
+import { useQuery } from "@tanstack/react-query";
+import SkeletonLoader from "../components/SkeletonLoader";
 
 const AllMealsPage = () => {
-
-  
   const { data, error, status } = useQuery({
     queryKey: ["all meals"],
     queryFn: getAllMeals,
     staleTime: 1000 * 60 * 5,
-   
-})
+  });
 
+  if (status === "pending") {
+    return <SkeletonLoader />;
+  }
 
-if(status === 'pending') {
-   return ( <SkeletonLoader /> )
-}
-
-
-if(status === 'error') {
-   return ( <h1 className="text-3xl  bg-white text-darkBrown">Error loading meals: {error.message}</h1> )
-}
-
-
-
-
+  if (status === "error") {
+    return (
+      <h1 className="text-3xl  bg-white text-darkBrown">
+        Error loading meals: {error.message}
+      </h1>
+    );
+  }
 
   return (
-   
     <section className="mt-20 w-full p-6 bg-white rounded-t-xl flex justify-start items-start flex-col gap-8">
       <h1 className="text-2xl text-darkBrown">All Meals</h1>
-         <div className="grid grid-cols-auto-fill w-full gap-8 ">
-             { 
-                      data.map((meal) => (
-                        <Card key={meal.id} {...meal} />
-                    ))
-
-             }
-
-         </div>
+      <div className="grid grid-cols-auto-fill w-full gap-8 items-start">
+        {data.map((meal) => (
+          <Card key={meal.id} {...meal} />
+        ))}
+      </div>
     </section>
-  
-  )
-}
+  );
+};
 
-export default AllMealsPage
+export default AllMealsPage;
--- src/pages/SearchMealPage.jsx
+++ src/pages/SearchMealPage.jsx
@@ -1,48 +1,37 @@
-import Card from "../components/Card"
-import { useQuery } from "@tanstack/react-query"
-import SkeletonLoader from "../components/SkeletonLoader"
-import { getSearchMeal } from "../../api/getSearchMeal"
+import Card from "../components/Card";
+import { useQuery } from "@tanstack/react-query";
+import SkeletonLoader from "../components/SkeletonLoader";
+import { getSearchMeal } from "../../api/getSearchMeal";
 
 const SearchPage = ({ queryParams }) => {
-
-  
   const { data, status } = useQuery({
     queryKey: ["search", queryParams],
     queryFn: () => getSearchMeal(queryParams),
-    retry: 2
-   
-})
+    retry: 2,
+  });
 
+  if (status === "pending") {
+    return <SkeletonLoader />;
+  }
 
-if(status === 'pending') {
-   return ( <SkeletonLoader /> )
-}
-
-
-if(status === 'error') {
-   return ( <h1 className="text-3xl text-darkBrown bg-white">No Search Results for {`'${queryParams}'`}</h1> )
-}
-
-
-
-
+  if (status === "error") {
+    return (
+      <h1 className="text-3xl text-darkBrown bg-white">
+        No Search Results for {`'${queryParams}'`}
+      </h1>
+    );
+  }
 
   return (
-   
     <section className="mt-20 w-full p-6 bg-white rounded-t-xl flex justify-start items-start flex-col gap-8">
       <h1 className="text-2xl text-darkBrown capitalize">{queryParams}</h1>
-         <div className="grid grid-cols-auto-fill w-full gap-8 ">
-             { 
-                      data.map((meal) => (
-                        <Card key={meal.id} {...meal} />
-                    ))
-
-             }
-
-         </div>
+      <div className="grid grid-cols-auto-fill w-full gap-8 items-start ">
+        {data.map((meal) => (
+          <Card key={meal.id} {...meal} />
+        ))}
+      </div>
     </section>
-  
-  )
-}
+  );
+};
 
-export default SearchPage
+export default SearchPage;
